365b45f0adfbfc754e7307d08f4be68e2e2d32d1
Justin Adkins <justinadkins25@yahoo.com>
2025-07-04 21:50:34 -0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,106 @@
+import 'package:deeptrainfront/screens/about_screen.dart';
+import 'package:deeptrainfront/screens/contact_screen.dart';
+import 'package:deeptrainfront/screens/features_screen.dart';
+import 'package:deeptrainfront/screens/pricing_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'auth/auth_reset_password.dart';
+
+
+import 'screens/home_screen.dart';
+import 'screens/dashboard_screen.dart';
+import 'screens/scenario_builder_screen.dart';
+import 'screens/simulator_screen.dart';
+import 'screens/kpi_dashboard_screen.dart';
+import 'auth/login_screen.dart';
+import 'auth/register_screen.dart';
+import 'auth/auth_confirm_screen.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'dashboard',
+          builder: (context, state) => const DashboardScreen(),
+        ),
+        GoRoute(
+          path: 'Builder',
+          builder: (context, state) => const ScenarioBuilderScreen(initialDomain: 'Healthcare'),
+        ),
+        GoRoute(
+          path: 'simulator',
+          builder: (context, state) => const SimulatorScreen(),
+        ),
+        GoRoute(
+          path: 'kpi',
+          builder: (context, state) => const KpiDashboardScreen(),
+        ), 
+        
+      ],
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/signUp',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+    GoRoute(
+      path: '/features',
+      builder: (context, state) => const FeaturesScreen(),
+    ),
+    GoRoute(
+      path: '/pricing',
+      builder: (context, state) => const PricingScreen(),
+    ),
+     GoRoute(
+      path: '/about',
+      builder: (context, state) => const AboutScreen(),
+    ),
+    GoRoute(
+      path: '/contact',
+      builder: (context, state) => const ContactScreen(),
+    ),
+    GoRoute(
+      path: '/confirm',
+      builder: (context, state) {
+        final email = state.extra as String;
+        return AuthConfirmScreen(email: email);
+      },
+    ),
+GoRoute(
+  path: '/reset-password',
+  builder: (context, state) => const ResetPasswordScreen(),
+),
+
+
+
+  ],
+);
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'DeepTrain',
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+    );
+  }
+}
