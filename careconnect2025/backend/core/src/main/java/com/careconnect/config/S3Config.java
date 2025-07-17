d5c7759c0c516c74a0c74ebd9607fd82ce5fb069
Ashenafi-Tesfaye <65870089+Ashenafi-Tesfaye@users.noreply.github.com>
2025-07-16 20:17:36 -0400

    import issue

--- careconnect2025/backend/core/src/main/java/com/careconnect/config/S3Config.java
+++ careconnect2025/backend/core/src/main/java/com/careconnect/config/S3Config.java
@@ -1,18 +1,19 @@
 package com.careconnect.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import com.careconnect.dto.S3Props;
 
 @Configuration
-@RequiredArgsConstructor
 public class S3Config {
 
-    private final S3Props s3Props;        
+    @Autowired
+    private S3Props s3Props;        
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
