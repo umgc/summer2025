c63e30c75a11b31cf971538119269af53f43c183
Raphael Edwenson <edwensonl@hotmail.com>
2025-07-20 21:37:12 -0400

    386 cc implement aws serverless adapter (#387)
    * cc-386 commenting out webclient bean
    
    * cc-386
    * handling dependency and servlet dispatcher conflicts
    * Adding build profiles
    * Commenting out some codes - For FitbitService for now
    
    * cc-386
    * Adding Lambda Handler and FallbackController

--- careconnect2025/backend/core/src/main/java/com/careconnect/config/WebClientConfig.java
+++ careconnect2025/backend/core/src/main/java/com/careconnect/config/WebClientConfig.java
@@ -3,15 +3,15 @@ package com.careconnect.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-    @Bean
+    /*@Bean
     public WebClient webClient() {
         return WebClient.builder().build();
-    }
+    }*/
 
     @Bean
     public RestTemplate restTemplate() {
