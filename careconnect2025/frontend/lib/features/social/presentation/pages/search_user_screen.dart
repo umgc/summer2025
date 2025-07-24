a1620bfd9b3fa1b7d4f71bd3d2bb80bfa3b91497
Lukecurran5 <89559696+Lukecurran5@users.noreply.github.com>
2025-07-24 00:22:18 -0400

    Merge 1 of Ashenafi's Code from AG-2 (#399)
    * Initial file changes
    
    * Fix chat file errors
    
    * Fix more file issues
    
    ---------
    
    Co-authored-by: eraphael <eraphael3@student.umgc.edu>

--- careconnect2025/frontend/lib/features/social/presentation/pages/search_user_screen.dart
+++ careconnect2025/frontend/lib/features/social/presentation/pages/search_user_screen.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
 import 'package:care_connect_app/services/api_service.dart';
+import 'package:care_connect_app/widgets/app_bar_helper.dart';
+import 'package:care_connect_app/widgets/common_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SearchUserScreen extends StatefulWidget {
-  const SearchUserScreen({super.key});
+  final int userId;
+  const SearchUserScreen({super.key, required this.userId});
 
   @override
   State<SearchUserScreen> createState() => _SearchUserScreenState();
@@ -85,7 +88,8 @@ class _SearchUserScreenState extends State<SearchUserScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Search Users')),
+      appBar: AppBarHelper.createAppBar(context, title: 'Search Users'),
+      drawer: const CommonDrawer(currentRoute: '/search-users'),
       body: Column(
         children: [
           Padding(
