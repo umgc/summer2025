a3a6b201909011225dc5d546a5ba1aa36beebcdc
Dat Truong <dtruong5@student.umgc.edu>
2025-07-23 23:09:14 -0500

    WIP: Progress on messaging, friends, and social networking modules

--- careconnect2025/backend/core/src/main/java/com/careconnect/repository/FriendshipRepository.java
+++ careconnect2025/backend/core/src/main/java/com/careconnect/repository/FriendshipRepository.java
@@ -1,2 +1,9 @@
-package com.careconnect.repository;public interface FriendshipRepository {
+package com.careconnect.repository;
+
+import com.careconnect.model.Friendship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 }
