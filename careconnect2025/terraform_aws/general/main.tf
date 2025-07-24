536ca4ca6925d2b17d19c1f404ff34beb6130d14
Raphael Edwenson <edwensonl@hotmail.com>
2025-07-24 18:53:19 +0000

    391 cc lambda integration with apigw (#408)
    * 391-cc adding cors and log format update
    
    * cc-391 Updating the Parameter store resources
    
    * cc-391 getting rid of vpc endpoint since we will use NAT Gateway
    * Moving apigw vpclink in vpc module
    
    * cc-391 removing integration from apigw
    
    * cc-391 adding api gw integration with the lambda
    * removing apigw vpc_link, no longer needed
    
    ---------
    
    Co-authored-by: eraphael <eraphael3@student.umgc.edu>

--- careconnect2025/terraform_aws/general/main.tf
+++ careconnect2025/terraform_aws/general/main.tf
@@ -44,7 +44,7 @@ module "ssm" {
   source                = "./modules/ssm"
   default_tags          = var.default_tags
   params_keys           = local.params_keys
-  cc_sensitive_params   = var.cc_ssm_params
+  cc_sensitive_params   = merge(var.cc_ssm_params, {JDBC_URI="jdbc:mysql://${module.rds.cc_db_endpoint}/${module.rds.cc_db_name}"})
 }
 
 module "iam" {
